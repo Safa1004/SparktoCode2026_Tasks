ed276d91f58dcc8e5d7002ac1ed01554f4ece525
Safa <safaa30796@gmail.com>
2026-07-02 09:55:03 +0400

    Task 1 solution

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Task 1 (Countdown Timer)
+        Console.Write("Enter a starting number: ");
+        int start = int.Parse(Console.ReadLine());
+        for (int i = start; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Liftoff!");
+    }
+}
