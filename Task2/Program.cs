e47bc1f77020d526b5c67c6ea563f65c43ca90ee
Safa <safaa30796@gmail.com>
2026-07-02 10:02:35 +0400

    Task2 solution

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,13 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Task 1 (Countdown Timer)
+        /*//Task 1 (Countdown Timer)
         Console.Write("Enter a starting number: ");
         int start = int.Parse(Console.ReadLine());
         for (int i = start; i >= 1; i--)
         {
             Console.WriteLine(i);
         }
-        Console.WriteLine("Liftoff!");
+        Console.WriteLine("Liftoff!"); */
+        //////////////////////////////////////////////////////////////////////
+        
+        //Task 2 (Sum of Numbers 1 to N)
+        Console.Write("Enter a positive whole number N:");
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
