6df43741fb73edb06776e8b706cf6d4ce18f1c39
Safa <safaa30796@gmail.com>
2026-07-05 14:50:58 +0400

    task 1 solution

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TASK 1 (Absolute Difference)
+        Console.Write("Enter the first number: ");
+        double firstNumber = double.Parse(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        double secondNumber = double.Parse(Console.ReadLine());
+        double result = firstNumber - secondNumber;
+        double positiveDiffrence = Math.Abs(result);
+        Console.WriteLine($"the positive diffrence is {positiveDiffrence}");
+
+    }
+}
