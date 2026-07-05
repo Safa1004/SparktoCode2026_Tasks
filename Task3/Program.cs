0e566d5ced460afd208d4047994da71b6a8f09de
Safa <safaa30796@gmail.com>
2026-07-05 14:58:16 +0400

    Task 2 solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,14 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        //TASK 1 (Absolute Difference)
+       /* //TASK 1 (Absolute Difference)
         Console.Write("Enter the first number: ");
         double firstNumber = double.Parse(Console.ReadLine());
         Console.Write("Enter the second number: ");
         double secondNumber = double.Parse(Console.ReadLine());
         double result = firstNumber - secondNumber;
         double positiveDiffrence = Math.Abs(result);
-        Console.WriteLine($"the positive diffrence is {positiveDiffrence}");
+        Console.WriteLine($"the positive diffrence is {positiveDiffrence}");*/
+       ////////////////////////////////////////////////////////////////////////////
+       
+       //Task 2 (Power & Root Explorer)
+       Console.Write("Enter a number: ");
+       double num = double.Parse(Console.ReadLine());
+       double sqr = Math.Pow(num, 2);
+       double sqrRoot = Math.Sqrt(num);
+       Console.WriteLine($"The square is: {sqr}");
+       Console.WriteLine($"The square root is: {sqrRoot}");
+
 
     }
 }
