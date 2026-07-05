2c1ee70afc33e0dd4a4e83ac27ec52eac87f1115
Safa <safaa30796@gmail.com>
2026-07-05 15:05:33 +0400

    Task 3 solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -14,13 +14,25 @@ class Program
         Console.WriteLine($"the positive diffrence is {positiveDiffrence}");*/
        ////////////////////////////////////////////////////////////////////////////
        
-       //Task 2 (Power & Root Explorer)
+       /* //Task 2 (Power & Root Explorer)
        Console.Write("Enter a number: ");
        double num = double.Parse(Console.ReadLine());
        double sqr = Math.Pow(num, 2);
        double sqrRoot = Math.Sqrt(num);
        Console.WriteLine($"The square is: {sqr}");
-       Console.WriteLine($"The square root is: {sqrRoot}");
+       Console.WriteLine($"The square root is: {sqrRoot}"); */
+       ////////////////////////////////////////////////////////////////////////////
+       
+       // Task 3 ( Name Formatter)
+       Console.Write("Please, Enter your full name: ");
+       string fullName = Console.ReadLine();
+       string upperFullName = fullName.ToUpper();
+       string lowerFullName = fullName.ToLower();
+       int fullNmaeLength = fullName.Length;
+       Console.WriteLine($"Your full name in uppercase: {upperFullName}");
+       Console.WriteLine($"Your full name in lowercase: {lowerFullName}");
+       Console.WriteLine($"The number of characters in your full name is: {fullNmaeLength}");
+       
 
 
     }
