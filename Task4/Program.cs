00e60e3dfe3424316e0b5a1e7df3fe9b28daca3a
Safa <safaa30796@gmail.com>
2026-07-06 17:21:42 +0400

    Task 1 solution

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4;
+
+class Program
+{
+    //Task 1 ( Personalized Welcome Function)
+    public static void PrintWelcome(string name)
+    {
+        Console.WriteLine($"Welcome balck! {name}");
+        
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        PrintWelcome(userName);
+        
+    }
+}
