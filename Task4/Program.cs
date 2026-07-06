b660906ef8bec2783638aa6b3560ab54a85ddfec
Safa <safaa30796@gmail.com>
2026-07-06 17:23:04 +0400

    commenting task 1 solution

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,17 +2,19 @@
 
 class Program
 {
-    //Task 1 ( Personalized Welcome Function)
+    /*//Task 1 ( Personalized Welcome Function)
     public static void PrintWelcome(string name)
     {
         Console.WriteLine($"Welcome balck! {name}");
         
-    }
+    }*/
     static void Main(string[] args)
     {
+        /*//Task 1 ( Personalized Welcome Function)
         Console.Write("Enter your name: ");
         string userName = Console.ReadLine();
-        PrintWelcome(userName);
+        PrintWelcome(userName);*/
+        
         
     }
 }
