f74f3eed443758ec8bd02ffdf89ec84ce93c8f93
Safa <safaa30796@gmail.com>
2026-07-06 17:41:00 +0400

    Task 3 solution

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -15,6 +15,13 @@ class Program
         // Square means multiply a number by itself.
         return number * number;
     }*/
+    //--------------------------------------------------------------
+    //Task 3 (Celsius to Fahrenheit Function)
+    public static double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+    
     static void Main(string[] args)
     {
         /*//Task 1 ( Personalized Welcome Function)
@@ -28,6 +35,13 @@ class Program
         int input = int.Parse(Console.ReadLine());
         int result = Square(input);
         Console.WriteLine($"The square of {input} is {result}.");*/
+        
+        //--------------------------------------------------------------
+        //Task 3 (Celsius to Fahrenheit Function)
+        Console.Write("Enter temperature in Celsius: ");
+        double celsiusInput = double.Parse(Console.ReadLine());
+        double fahrenheit = CelsiusToFahrenheit(celsiusInput);
+        Console.WriteLine($"{celsiusInput} celsius is equal to {fahrenheit} fahrenheit.");
         
         
         
