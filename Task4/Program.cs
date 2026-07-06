0f47769fb67ce22cc9b380f7b0be8c0e97b0b42a
Safa <safaa30796@gmail.com>
2026-07-06 17:35:03 +0400

    Task 2 solution

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -8,12 +8,27 @@ class Program
         Console.WriteLine($"Welcome balck! {name}");
         
     }*/
+    //--------------------------------------------------------------
+    //Task 2 ( Square Number Function)
+    public static int Square(int number)
+    {
+        // Square means multiply a number by itself.
+        return number * number;
+    }
     static void Main(string[] args)
     {
         /*//Task 1 ( Personalized Welcome Function)
         Console.Write("Enter your name: ");
         string userName = Console.ReadLine();
         PrintWelcome(userName);*/
+        
+        //--------------------------------------------------------------
+        //Task 2 ( Square Number Function)
+        Console.Write("Enter a number: ");
+        int input = int.Parse(Console.ReadLine());
+        int result = Square(input);
+        Console.WriteLine($"The square of {input} is {result}.");
+        
         
         
     }
