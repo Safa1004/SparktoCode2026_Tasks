e278322c8b21567831a6db5e34ad20eecf9268e7
Safa <safaa30796@gmail.com>
2026-07-07 16:47:23 +0400

    Task 1 solution

---
+++ Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Task 1 (Fixed Grades Array)
+        int [] grades = new int [5];
+        // for loop to get input from the user and store each grade
+        for (int i = 0 ; i < grades.Length ; i++)
+        {
+            Console.Write("Enter grade " + (i+1) + ": ");
+            grades[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nThe grades you entered are:");
+        // foreach loop to print every grade in the array
+        foreach (int grade in grades)
+        {
+            Console.WriteLine(grade);
+        }
+
+    }
+}
