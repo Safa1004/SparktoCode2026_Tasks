681835279a28140da0f38be81fc255f6788801fe
Safa <safaa30796@gmail.com>
2026-07-09 11:44:46 +0400

    Store more than one customer (list)

---  MiniCompoundProject1/Program.cs
+++  MiniCompoundProject1/Program.cs
@@ -6,18 +6,30 @@ class Program
 {
     static void Main(string[] args)
     { 
-        //Gathering customer info 
+        //Store more than one customer (list)
+        List<string> customerNames = new List<string>();
+        List<string> accountNumbers = new List<string>();
+        List<double> balances = new List<double>();
+        
+        //Gathering customer info --first step 
         Console.Write("Enter customer name: ");
         string customerName = Console.ReadLine();
         Console.Write("Enter account number: ");
         string accountNumber = Console.ReadLine();
         Console.Write("Enter starting balance: ");
-        double balance = double.Parse(Console.ReadLine());
+        double startingBalance = double.Parse(Console.ReadLine());
         
         Console.WriteLine("\n--- Account Created ---");
         Console.WriteLine($"Name: {customerName}");
         Console.WriteLine($"Account Number:{accountNumber} ");
-        Console.WriteLine($"Balance: {balance}");
+        Console.WriteLine($"Balance: {startingBalance}");
+        
+        // .Add() puts the new value onto the end of the list
+        // adding to all 3 lists right after each other, in the same order 
+        // so they stay the same length and stay lined up
+        customerNames.Add(customerName);
+        accountNumbers.Add(accountNumber);
+        balances.Add(startingBalance);
     }
 
 }
