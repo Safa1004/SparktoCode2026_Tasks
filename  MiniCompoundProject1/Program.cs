29f741e117b76fed20e9884cd8de654c30a48255
Safa <safaa30796@gmail.com>
2026-07-09 11:31:15 +0400

    Gathering customer info

---
+++  MiniCompoundProject1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MiniCompoundProject1;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        //Gathering customer info 
+        Console.Write("Enter customer name: ");
+        string customerName = Console.ReadLine();
+        Console.Write("Enter account number: ");
+        string accountNumber = Console.ReadLine();
+        Console.Write("Enter starting balance: ");
+        double balance = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("\n--- Account Created ---");
+        Console.WriteLine($"Name: {customerName}");
+        Console.WriteLine($"Account Number:{accountNumber} ");
+        Console.WriteLine($"Balance: {balance}");
+    }
+
+}
