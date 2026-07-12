fcc4dc85365b9a6d9f3b69446642ecffb0b0aa09
Safa <safaa30796@gmail.com>
2026-07-12 14:52:54 +0400

    adding BankAccount class with properties

---
+++ Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task6;
+
+
+// BankAccount class ( holds account info and handles deposits/withdrawals)
+// using auto-properties ( { get; set; } ) instead of plain fields for
+// AccountNumber, HolderName, Balance - same end result as a public field,
+//  just the "official" way to expose data on a class
+
+class BankAccount
+{
+    
+   public int AccountNumber { get; set; }
+   public string HolderName { get; set; } 
+   public double Balance { get; set; }
+  
+   
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
