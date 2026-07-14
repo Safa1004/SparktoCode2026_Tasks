eeb240c3c726d76e22a8c1ea98fbe2770397a7d9
Safa <safaa30796@gmail.com>
2026-07-14 15:06:59 +0400

    Room class

---
+++ Task7/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task7;
+
+class Room
+{
+    public int RoomNumber { get; set; }
+    public string RoomType { get; set; }
+    public double PricePerNight { get; set; }
+    public bool IsAvailable { get; set; }
+    
+    // constructor
+    // IsAvailable isn't a param - hardcoded true, task says a new room is
+    // always available when first added
+    // values passed in at once
+    public Room(int roomNumber, string roomType, double pricePerNight) // Params 
+    {
+        RoomNumber = roomNumber;
+        RoomType = roomType;
+        PricePerNight = pricePerNight;
+        IsAvailable = true;
+    }
+    // method 
+    public void displayRoom()
+    {
+        // ternary operator (shorter) 
+        string status = IsAvailable ? "Available" : "Booked";
+        Console.WriteLine($"Room {RoomNumber} | {RoomType} | OMR {PricePerNight:F2}/night | {status}");// F2= 2 decimal places
+        
+    
+    }
+    
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
