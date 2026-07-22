93060438c35dbcef6e1f3e3ac54a624fc926fe16
Safa <safaa30796@gmail.com>
2026-07-22 16:38:44 +0400

    DEPARTMENT (created WITHOUT the Mgr_ssn FK constraint first)

--- DB_Tasks/DBTask.sql
+++ DB_Tasks/DBTask.sql
@@ -2,3 +2,19 @@ CREATE DATABASE CompanyDB;
 GO
 USE CompanyDB;
 GO --closes the batch 
+-- ------------------------------------------------------------
+-- DEPARTMENT (created WITHOUT the Mgr_ssn FK constraint first)
+-- ------------------------------------------------------------
+CREATE TABLE Department (
+                            Dnumber INT NOT NULL ,
+                            Dname VARCHAR(50) NOT NULL ,
+                            Mgr_ssn CHAR(9) NULL, --maybe a department may not have assigned manager yet
+                            Mgr_start_date DATE NULL,
+                            NumberOfEmployees INT NOT NULL DEFAULT 0,
+                            CONSTRAINT PK_Department PRIMARY KEY (Dnumber),
+                            CONSTRAINT UQ_Department_Name UNIQUE (Dname) --alternative key, no duplicates
+
+
+
+);
+GO
